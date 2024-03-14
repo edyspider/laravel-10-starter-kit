765ae93f35ec64586773b14ebec21756a2042f9f
EdySpider <eder.silva84@gmail.com>
2024-03-14 11:12:39 +0000

    update welcome page

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'home');
+Route::view('/', 'home')
+    ->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
